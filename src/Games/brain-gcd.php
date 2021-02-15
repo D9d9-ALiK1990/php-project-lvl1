dca8f2c3de35636a045e405830c2a75e9ba7daf2
otmakhov <otmakhov1990@gmail.com>
2021-02-16 00:55:18 +0700

    kostyl

--- src/Games/brain-gcd.php
+++ src/Games/brain-gcd.php
@@ -11,7 +11,23 @@ function ask(): array
     $number1 = random_int(1, 100);
     $number2 = random_int(1, 100);
     $expression = $number1 . ' ' . $number2;
-    $result = gmp_gcd($number1, $number2);
+    if ($number1 < $number2) {
+        $nod = $number1;
+    } else {
+        $nod = $number2;
+    }
+    $i = $number1 % $nod;
+    $j = $number2 % $nod;
+    echo $i;
+    echo $j;
+    while($i + $j !== 0 ) {
+        $nod--;
+        $i = $number1 % $nod;
+        $j = $number2 % $nod;
+    }
+
+    $result = $nod;
+    //$result = gmp_gcd($number1, $number2);
     $arr[] = $expression;
     $arr[] = $result;
     return $arr;
